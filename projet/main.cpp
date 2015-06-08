75864136fc0080231f0668e0acb616c6e8802c4c
ArthurPons <pons.arthur@gmail.com>
2015-06-08 23:12:47 +0200

    Avancement ajout precedence tache composite

--- projet/main.cpp
+++ projet/main.cpp
@@ -16,19 +16,22 @@ int main(int argc, char *argv[])
     ProjetManager& pm=ProjetManager::Instance();
 
     Projet* projet1=pm.createProjet("Projet1","DescriptionProjet1");
-    Projet* projet2=pm.createProjet("Projet2","DescriptionProjet2");
+    //Projet* projet2=pm.createProjet("Projet2","DescriptionProjet2");
 
-    TacheUnitaire* tache1=projet2->addTacheUnitaire("t1","titre1","description1",30,1);
+    TacheUnitaire* tache1=projet1->addTacheUnitaire("t1","titre1","description1",30,1);
     TacheUnitaire* tache2=projet1->addTacheUnitaire("t2","titre2","description2",30,1);
     TacheComposite* tache3=projet1->addTacheComposite("t3","titre3","desc3");
     TacheComposite* tache4=projet1->addTacheComposite("t4","titre4","desc4");
 
     tache1->addPrecedence(tache2);
-    tache1->addPrecedence(tache3);
 
+
+
+    /*
     QVector<Tache*> precedencesTache1=tache1->getListeTachesMeresPrecedence();
     for(int i=0;i<precedencesTache1.size();i++)
         qDebug()<<"La tache"<<precedencesTache1.at(i)->getIdentifiant()<<"precede la tache"<<tache1->getIdentifiant();
+    */
 
     return a.exec();
 }
--- projet/tache.cpp
+++ projet/tache.cpp
@@ -27,23 +27,58 @@ void Tache::setTacheMereComposite(TacheComposite *mere)
 
 void Tache::addPrecedence(TacheComposite* precedence)
 {
+    QVector<Tache*> listeSousTachesPrecedence=precedence->getListeSousTaches();
+
+    if(this==precedence){
+        qDebug()<<"La tache composite"<<identifiant<<"ne peut etre une precedence"
+                <<"d'elle meme";
+        return;
+    }
+
+    for(int i=0;listeSousTachesPrecedence.size();i++)
+        if(this==listeSousTachesPrecedence.at(i)){
+            qDebug()<<"La tache composite"<<identifiant<<"ne peut etre une precedence"
+                    <<"d'elle meme";
+            return;
+        }
+
     for(int i=0;i<listeTachesMeresPrecedence.size();i++){
         if(listeTachesMeresPrecedence.at(i)==precedence){
             qDebug()<<"La tache"<<listeTachesMeresPrecedence.at(i)->getIdentifiant()
-                    <<"est deja une precedence de la tache"<<getIdentifiant();
+                    <<"est deja une precedence de la tache"<<getIdentifiant()
+                    <<"impossible de rajouter"<<precedence->getIdentifiant()
+                    <<"en tant que precedence";
             return;
         }
-        QVector<Tache*> listeSousTachesPrecedence=precedence->getListeSousTaches();
-        for(i=0;i<listeSousTachesPrecedence.size();i++){
-
+        for(int j=0;j<listeSousTachesPrecedence.size();j++){
+            if(listeTachesMeresPrecedence.at(i)==listeSousTachesPrecedence.at(j)){
+                qDebug()<<"La tache"<<listeSousTachesPrecedence.at(j)->getIdentifiant()
+                        <<"est deja une precedence de la tache"<<getIdentifiant()
+                        <<"impossible de rajouter"<<precedence->getIdentifiant()
+                        <<"en tant que precedence";
+                return;
+            }
         }
     }
+
     listeTachesMeresPrecedence.append(precedence);
-    qDebug()<<"La tache composite"<<precedence->getIdentifiant()<<"a ete ajoute en tant que precedence de"<<identifiant;
+    qDebug()<<"La tache composite"<<precedence->getIdentifiant()
+            <<"a ete ajoute en tant que precedence de"<<identifiant;
+    for(int j=0;j<listeSousTachesPrecedence.size();j++){
+        listeTachesMeresPrecedence.append(listeSousTachesPrecedence.at(j));
+        qDebug()<<"La tache"<<listeSousTachesPrecedence.at(j)->getIdentifiant()
+                <<"a ete ajoute en tant que precedence de"<<identifiant;
+    }
 }
 
 void Tache::addPrecedence(TacheUnitaire* precedence)
 {
+    if(this==precedence){
+        qDebug()<<"La tache"<<identifiant<<"ne peut etre une precedence"
+                <<"d'elle meme";
+        return;
+    }
+
     for(int i=0;i<listeTachesMeresPrecedence.size();i++)
         if(listeTachesMeresPrecedence.at(i)==precedence){
             qDebug()<<"La tache"<<listeTachesMeresPrecedence.at(i)->getIdentifiant()
